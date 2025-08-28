bc4c42d9a8f463ed2784bc633e4c3b21bcb6e5ee
sharatpotukuchi <134854946+sharatpotukuchi@users.noreply.github.com>
2025-08-28 17:40:58 +1000

    Create models.py

---
+++ bridge/models.py
@@ -0,0 +1,13 @@
+from otree.api import *
+
+class C(BaseConstants):
+    NAME_IN_URL = 'bridge'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 1
+
+class Subsession(BaseSubsession): pass
+class Group(BaseGroup): pass
+
+class Player(BasePlayer):
+    pid = models.StringField()
+    cond = models.IntegerField(choices=[0,1], initial=0)
