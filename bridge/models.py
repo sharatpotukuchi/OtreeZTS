d2583fa4553b06451812ec511f063d89bf5f5416
sharatpotukuchi <134854946+sharatpotukuchi@users.noreply.github.com>
2025-08-28 20:16:37 +1000

    Update models.py

--- bridge/models.py
+++ bridge/models.py
@@ -1,4 +1,5 @@
 from otree.api import *
+from django.db import models as djm
 
 class C(BaseConstants):
     NAME_IN_URL = 'bridge'
@@ -10,5 +11,14 @@ class Group(BaseGroup): pass
 
 class Player(BasePlayer):
     pid  = models.StringField()
-    cond = models.IntegerField(choices=[0,1], initial=0)
+    cond = models.IntegerField(choices=[0, 1], initial=0)
     round_index = models.IntegerField(initial=1)
+
+# Local assignment store so Qualtrics can POST directly into oTree
+class Assignment(djm.Model):
+    pid = djm.CharField(primary_key=True, max_length=128)
+    cond = djm.PositiveSmallIntegerField()
+    assigned_at = djm.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'assignments'
